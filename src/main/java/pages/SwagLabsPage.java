9137b79d0313faca8f4e31f04ecc68d56f6593a2
Asem Asfandiarova <assemsim@gmail.com>
2025-03-06 19:14:20 +0600

    fix: make driver final

--- src/main/java/pages/SwagLabsPage.java
+++ src/main/java/pages/SwagLabsPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SwagLabsPage {
-    private WebDriver driver;
+    private final WebDriver driver;
     @FindBy(xpath = "//*[@class='header_label']/div[@class='app_logo']")
     private WebElement swagLabsDiv;
     @FindBy(xpath = "//*[@id='inventory_container']")
